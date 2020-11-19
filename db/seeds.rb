6367662a2d3953383124015c127c3b742965cde4
Ken Wall <kwall925@gmail.com>
2020-11-19 19:20:20 -0300

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+ingredients = JSON.parse(open(url).read)
+
+ingredients["drinks"].each do |ing|
+  Ingredient.create(name: ing["strIngredient1"])
+end
