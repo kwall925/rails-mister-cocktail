428ad5aede3fd094eb9e52712077e5618f70bc3f
Ken Wall <kwall925@gmail.com>
2020-11-20 16:43:36 -0300

    views set up

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,28 @@
+class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktails_path
+  end
+
+  def show
+    @cocktails = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  private 
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+end
